6fa08ea7881c4c00ffc152a7b042c3f0789a2192
I057386 <I057386@AGRN56583514A.apj.global.corp.sap>
2020-09-22 16:40:42 +0530

    Initial Commit

---
+++ src/main/java/root/controller/LoginController.java
@@ -0,0 +1,26 @@
+package root.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class LoginController
+{
+	
+	@GetMapping("/myLoginPage")
+	public String showLoginPage(
+	)
+	{
+		// return "fancy-login";
+		
+		return "login-metro";
+	}
+	
+	@GetMapping("/access-denied")
+	public String showaccessDenied(
+	)
+	{
+		return "access-denied";
+	}
+	
+}
